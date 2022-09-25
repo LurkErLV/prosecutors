cae0e0d6459f659f56e74bd40687383c1a3f8332
Alberts Matrozis <62105039+LurkErLV@users.noreply.github.com>
2022-09-25 17:35:47 +0300

    upd

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 'use strict';
 
 const express = require('express');
-const { port, ip } = require('../config.json');
+const http = require('http');
+const https = require('https');
+const { port, ip } = require('./config.json');
 const app = express();
 const session = require('express-session');
 const passport = require('passport');
-const discordStrategy = require('../strategies/discordstrategy');
-const db = require('../database/database');
+const discordStrategy = require('./strategies/discordstrategy');
+const db = require('./database/database');
 const path = require('path');
 const bodyParser = require('body-parser');
 const { check, validationResult } = require('express-validator');
-const Logs = require('../models/Logs');
+const Logs = require('./models/Logs');
 const compression = require('compression')
 const serverless = require('serverless-http');
 const router = express.Router();
 
 db.then(() => console.log('Connected to MongoDB')).catch(err => console.log(err));
 
+const httpServer = http.createServer(app);
+
 // Routes
-const authRoute = require('../routes/auth');
-const profileRoute = require('../routes/profile');
-const dashboardRoute = require('../routes/dashboard');
-const instructorsRoute = require('../routes/instructors');
-const academyRoute = require('../routes/academy');
+const authRoute = require('./routes/auth');
+const profileRoute = require('./routes/profile');
+const dashboardRoute = require('./routes/dashboard');
+const instructorsRoute = require('./routes/instructors');
+const academyRoute = require('./routes/academy');
 
 app.use(session({
   secret: '228secret1337',
@@ -34,7 +38,7 @@ app.use(session({
 }));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -57,8 +61,6 @@ app.get('/', (req, res) => {
 });
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 
-module.exports = app;
-module.exports.handler = serverless(app);
+httpServer.listen(8080);
