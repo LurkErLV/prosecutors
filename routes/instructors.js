0a43264fe27d58ada42642cbf9563e48e5950b4b
Alberts Matrozis <62105039+LurkErLV@users.noreply.github.com>
2022-09-25 18:42:26 +0300

    update

--- routes/instructors.js
+++ routes/instructors.js
@@ -9,9 +9,10 @@ router.get('/', (req, res) => {
         const curator_sa = await db.collection('users').find({ position: "Curator SA" }).toArray();
         const head_sa = await db.collection('users').find({ position: "Head SA" }).toArray();
         const d_head_sa = await db.collection('users').find({ position: "D.Head SA" }).toArray();
+        const high_instrucor_sa = await db.collection('users').find({ position: "High Instructor SA" });
         const instructor_sa = await db.collection('users').find({ position: "Instructor SA" }).toArray();
 
-        const sa = curator_sa.concat(head_sa).concat(d_head_sa).concat(instructor_sa);
+        const sa = curator_sa.concat(head_sa).concat(d_head_sa).concat(high_instrucor_sa).concat(instructor_sa);
 
         res.render('instructors', {
             user: req.user,
